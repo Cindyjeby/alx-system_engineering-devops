dae5774bcd038e0130e5f7b601e0e10a11c25765
cindyjeby <cindyjeby@gmail.com>
2023-09-27 12:32:55 -0700

    advanced

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,78 @@
+# add stable version of nginx
+exec { 'add nginx stable repo':
+command => 'sudo add-apt-repository ppa:nginx/stable',
+path => '/usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin',
+}
+
+#software update
+exec { 'update packages':
+command => 'apt-get update',
+path=> '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+#nginx install
+package { 'nginx':
+ensure => 'installed',
+}
+
+#allow HTTP
+exec { 'allow HTTP':
+command => "ufw allow 'Nginx HTTP'",
+path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+onlyif => '! dpkg -l nginx | egrep \'ii.*nginx\' > /dev/null 2>&1',
+}
+
+#change folder rights
+exec { 'chmod www folder':
+command => 'chmod -R 755 /var/www',
+path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# create index file
+file { '/var/www/html/index.html':
+content => "Hello World!\n",
+}
+
+#create index file
+file { '/var/www/html/404.html':
+content => "Ceci n'est pas une page\n",
+}
+
+# redirection and error page
+file { 'Nginx default config file':
+ensure => file,
+path => '/etc/nginx/site-enable/default',
+content =>
+"server {
+listen 80 default_server;
+listen[::]:80 default_server;
+root /var/www/html;
+#add index.php
+index index.html index.htm index.nginx-debian.html;
+server_name _;
+location / {
+#first attempt to serve request as file
+try_files \$uri \$uri \$uri/ =404;
+}
+error_page 404 /404.html;
+loaction /404.html {
+internal;
+}
+if (\$request_filename ~ redirect_me){
+rewite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+}
+}
+",
+}
+
+# restart nginx
+exec { 'restart service':
+command => 'service nginx restart',
+path => 'usr/bin:/usr/sbin:/bin',
+}
+
+#start service nginx
+service { 'nginx':
+ensure => running,
+require => Package['nginx'],
+}
