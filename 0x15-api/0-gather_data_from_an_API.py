29d01a8d7b16abb65d5b4f4a214f792751030289
cindyjeby <cindyjeby@gmail.com>
2023-10-24 20:06:21 -0700

    debug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,15 +5,20 @@ import requests
 import sys
 
 
-if __name == "__main__":
+if __name__ == "__main__":
     #define rest url
     url = "https://jsonplaceholder.typicode.com/"
+
     #retrieve user info
     user_info = requests.get(url + "users/{}".format{sys.argv[1])).json()
+
     #retrive to do lists
-    to_dos= requests.get(url + "todos", parms={"userId": sys.argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
     #
     completed = [t.get("title") for t in todos if t.get("completed") is True]
+
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
+
     [print("\t {}".format(c)) for c in completed]
