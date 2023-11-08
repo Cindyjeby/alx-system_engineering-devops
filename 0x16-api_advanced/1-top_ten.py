2917a46c49894717c5ccee6337eb6ff2a8e3f2b1
cindyjeby <cindyjeby@gmail.com>
2023-11-08 00:32:29 -0800

    debug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""sub reddit"""
+"""Query subreddit"""
 
 import requests
 
+
 def top_ten(subreddit):
-	"""query reddit and print titles of the first 10 hot posts"""
+	"""Query Reddit and print titles of the first 10 hot posts"""
 
 	url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
-	#set custiom user-agent to avoid too many requests error
+	# Set a custom User-Agent to avoid too many requests error
 	headers = {'User-Agent': 'My user Agent 1.0'}
 
-	#send GET request
+	# send a GET request to the Reddit API
 	response = requests.get(url, headers=headers, allow_redirects=False)
 
 	try:
@@ -19,8 +20,8 @@ def top_ten(subreddit):
 					allow_redirects=False)
 		if response.status_code == 200:
 			children = response.json().get('data').get('children')
-			for k = range(10):
-				print(children[k].get('data').get('title'))
+			for i in range(10):
+				print(children[i].get('data').get('title'))
 		else:
 			print("None")
 	except Exception:
